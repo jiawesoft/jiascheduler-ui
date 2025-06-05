a637b4440e45576e072e407c2d413cca62840274
iwannay <772648576@qq.com>
2025-06-05 17:36:16 +0800

    feat: add workflow api

---
+++ src/api/workflow.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+export interface WorkflowRecord {
+  id?: number;
+  name: string;
+  info: string;
+}
+
+export interface SaveWorkflowResp {
+  ret: number;
+}
+export type SaveWorkflowReq = Partial<WorkflowRecord>;
+
+export function saveWorkflow(data: SaveWorkflowReq) {
+  if (data?.id === 0) {
+    delete data.id;
+  }
+  return axios.post<SaveWorkflowResp>('/api/workflow/save', data);
+}
+
+export interface Task {
+  standard?: {
+    eid: string;
+  };
+  custom?: {
+    executor_id: number;
+    timeout?: number;
+    code: string;
+    upload_file: string;
+  };
+}
+
+export interface NodeConfig {
+  id: string;
+  name: string;
+  node_type: string;
+  task_type: 'standard|custom';
+  task: Task;
+  data: object;
+}
+
+export interface EdgeConfig {
+  name: string;
+}
+
+export interface SaveWorkflowVersionReq {
+  id?: number;
+  pid?: number;
+  name: string;
+  info?: string;
+  save_time: 'draft' | 'release';
+  nodes: NodeConfig[];
+  edges: EdgeConfig[];
+}
+
+export function saveWorkflowVersion(data: SaveWorkflowVersionReq) {
+  if (data?.id === 0) {
+    delete data.id;
+  }
+  return axios.post<SaveWorkflowResp>('/api/workflow/version/save', data);
+}
